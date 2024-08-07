43138ae1327ab4cd45527cb2a1e957ec2f03ad64
Jayson Camba <mjmcamba@gmail.com>
2024-08-07 20:44:23 +0800

    Add seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+books = JSON.parse(File.read(Rails.root.join("db/json_files/books.json")))["books"]
+publishers = {}
+authors = {}
+
+books.each do |book_hash|
+  book = Book.find_or_initialize_by(book_hash.except(*%w[authors publisher]))
+  publisher = publishers[book_hash["publisher"]] ||= Publisher.find_or_create_by(name: book_hash["publisher"]) 
+  book.publisher = publisher
+  book_hash["authors"].each do |author_hash|
+    author = authors[author_hash] ||= Author.find_or_create_by(author_hash)
+    book.authors << author
+  end
+  book.save!
+end
